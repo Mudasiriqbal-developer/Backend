96c27470b077e22236ec0701cdd21c4e38d1a44b
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-07-11 15:44:42 +0500

    Clean up user and video model schemas for consistency and readability

--- youtube-backend-project/src/models/video.model.js
+++ youtube-backend-project/src/models/video.model.js
@@ -1,46 +1,45 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new Schema( 
-    {
-        videoFile: {
-            type: String, // Cloudinary URL
-            required: true
-        },
-        thumbnail: {
-            type: String, // Cloudinary URL
-            required: true
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String,
-            required: true
-        },
-        duration: {
-            type: Number, // Cloudinary URL
-            required: true
-        },
-        views: {
-            type: Number, // Cloudinary URL
-            default: 0
-        },
-        isPubliched: {
-            type: Boolean,
-            default: true
-        },
-        owner: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-        }
-
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // Cloudinary URL
+      required: true,
     },
-    {
-        timestamps: true
-    }
-)   
+    thumbnail: {
+      type: String, // Cloudinary URL
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, // Cloudinary URL
+      required: true,
+    },
+    views: {
+      type: Number, // Cloudinary URL
+      default: 0,
+    },
+    isPubliched: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 videoSchema.plugin(mongooseAggregatePaginate);
 
