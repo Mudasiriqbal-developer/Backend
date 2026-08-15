cbaae59518c37424ae95c879695cde60532403d5
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-08-15 12:56:50 +0500

    Implement video filtering, pagination, and sorting in getAllVideos function

--- youtube-backend-project/src/controllers/video.controller.js
+++ youtube-backend-project/src/controllers/video.controller.js
@@ -10,11 +10,61 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
-})
+    const filter = {}
+    
+    // Filter by query if provided
+    if(query && query.trim() !== "") {
+        const q = query.trim()
+        filter.$or = [
+            { title: { $regex: q, $options: "i" }},
+            { description: { $regex: q, $options: "i" }}
+        ];
+    }
+
+    // Filter by userId if provided
+    if(userId && isValidObjectId(userId)) {
+        if(!isValidObjectId(userId)) {
+            throw new ApiError(400, "Invalid userId")
+        }
+    }
+
+    // Pagination and sorting
+     const pageNumber = Math.max(1, parseInt(page, 10));
+     const lim = Math.max(1, parseInt(limit, 10));
+     const skip = (pageNumber - 1) * lim;
+     const sort = {[sortBy]: sortType === "asc" ? 1 : -1}
+
+     const [videos, total] = await Promise.all([
+        Video.find(filter)
+        .sort(sort)
+        .skip(skip)
+        .limit(lim)
+        .populate("userId", "name email")
+        .lean(),
+        Video.countDocuments(filter)
+     ]);
+
+     const meta = {
+        total,
+        page: pageNumber,
+        limit: lim,
+        totalPages: Math.ceil(total / lim),
+     }
+
+     return res.status(200).json({
+        success: true,
+        data: {
+            videos,
+            meta
+        }
+     });
+
+});
 
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
